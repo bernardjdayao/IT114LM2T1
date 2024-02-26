4c2ddb038647828755ab2b6e40ffe3915b9e75dd
Bernard Justin Dayao <dbernardjustin@gmail.com>
2024-02-26 11:39:48 +0800

    Fixed Exercise 3

--- Module2Topic1/Exercise3.aspx.cs
+++ Module2Topic1/Exercise3.aspx.cs
@@ -12,6 +12,7 @@ namespace Module1Exercise1
         protected void Page_Load(object sender, EventArgs e)
         {
             // TODO 3.3 Set the text value of the finalGrade label to "Submit your grade percentage to see your final grade!". Watch out for post backs
+            finalGrade.Text = "Submit your grade percentage to see your final grade!";
         }
 
         protected void Unnamed_Click(object sender, EventArgs e)
